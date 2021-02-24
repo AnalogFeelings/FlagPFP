0b5c5121d2ada4ea92dd7c8fa7a8fc202e1e7ba1
Manuel (Aesthetical) <AestheticalGames@users.noreply.github.com>
2021-02-24 18:51:35 +0100

    First files.

---
+++ FlagColors.cs
@@ -0,0 +1,134 @@
+﻿namespace FlagPFP.Main
+{
+    public partial class MainProgram
+    {
+        public static (string color, string letter)[] gayFlag = new (string color, string letter)[]
+        {
+            ( "#FF0000", " " ),
+            ( "#FF8D00", " " ),
+            ( "#FFEE00", " " ),
+            ( "#00811A", " " ),
+            ( "#004BFF", " " ),
+            ( "#89008B", " " )
+        };
+
+        public static (string color, string letter)[] agenderFlag = new (string color, string letter)[]
+        {
+            ( "#000000", " " ),
+            ( "#BBBBBB", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#9AF57C", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#BBBBBB", " " ),
+            ( "#000000", " " )
+        };
+
+        public static (string color, string letter)[] aromanticFlag = new (string color, string letter)[]
+        {
+            ( "#00A735", " " ),
+            ( "#92D574", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#ABABAB", " " ),
+            ( "#000000", " " )
+        };
+
+        public static (string color, string letter)[] asexualFlag = new (string color, string letter)[]
+        {
+            ( "#000000", " " ),
+            ( "#A5A5A5", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#970083", " " ),
+        };
+
+        public static (string color, string letter)[] bisexualFlag = new (string color, string letter)[]
+        {
+            ( "#FA0073", " " ),
+            ( "#FA0073", " " ),
+            ( "#B14D9A", " " ),
+            ( "#0034AD", " " ),
+            ( "#0034AD", " " ),
+        };
+
+        public static (string color, string letter)[] demisexualFlag = new (string color, string letter)[]
+        {
+            ( "#FFFFFF", " " ),
+            ( "#000000", " " ),
+            ( "#810073", " " ),
+            ( "#D4D4D3", " " )
+        };
+
+        public static (string color, string letter)[] gaymenFlag = new (string color, string letter)[]
+        {
+            ( "#00A7D8", " " ),
+            ( "#00BBFF", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#00D58C", " " ),
+            ( "#00A536", " " )
+        };
+
+        public static (string color, string letter)[] genderfluidFlag = new (string color, string letter)[]
+        {
+            ( "#FF75A2", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#BE18D6", " " ),
+            ( "#000000", " " ),
+            ( "#333EBD", " " )
+        };
+
+        public static (string color, string letter)[] genderqueerFlag = new (string color, string letter)[]
+        {
+            ( "#B57EDC", " " ),
+            ( "#B57EDC", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#4A8123", " " ),
+            ( "#4A8123", " " )
+        };
+
+        public static (string color, string letter)[] lesbianFlag = new (string color, string letter)[]
+        {
+            ( "#D52D00", " " ),
+            ( "#FF9A56", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#D362A4", " " ),
+            ( "#A30262", " " )
+        };
+
+        public static (string color, string letter)[] nonbinaryFlag = new (string color, string letter)[]
+        {
+            ( "#FCF434", " " ),
+            ( "#FCFCFC", " " ),
+            ( "#9C59D1", " " ),
+            ( "#2C2C2C", " " )
+        };
+
+        public static (string color, string letter)[] pansexualFlag = new (string color, string letter)[]
+        {
+            ( "#FF218C", " " ),
+            ( "#FF218C", " " ),
+            ( "#FFD800", " " ),
+            ( "#FFD800", " " ),
+            ( "#21B1FF", " " ),
+            ( "#21B1FF", " " )
+        };
+
+        public static (string color, string letter)[] polysexualFlag = new (string color, string letter)[]
+        {
+            ("#F61CB9", " "),
+            ("#F61CB9", " "),
+            ("#07D569", " "),
+            ("#07D569", " "),
+            ("#1C92F6", " "),
+            ("#1C92F6", " ")
+        };
+
+        public static (string color, string letter)[] transgenderFlag = new (string color, string letter)[]
+        {
+            ( "#00D0FC", " " ),
+            ( "#FFABBA", " " ),
+            ( "#FFFFFF", " " ),
+            ( "#FFABBA", " " ),
+            ( "#00D0FC", " " )
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Pastel;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FlagPFP.Main
+{
+    public partial class MainProgram
+    {
+        //This program complies with the NO_COLOR standard as it uses the Pastel library.
+        static void Main(string[] args)
+        {
+            if(args.Length < 4)
+            {
+                Console.WriteLine("Error: ".Pastel(Color.DarkRed) +
+                    "Please follow the syntax: ".Pastel(Color.Red)
+                    + "flagpfp <image> <flag> <pixel margin> <output name>".Pastel(Color.CadetBlue));
+                Environment.Exit(1);
+            }
+
+            Bitmap imageFile = null;
+            Bitmap flag = null;
+            string flagPath;
+            int pixelMargin = 0;
+
+            try
+            {
+                imageFile = new Bitmap(Image.FromFile(args[0]));
+                flagsTable.TryGetValue(args[1], out flagPath);
+                flag = new Bitmap(Image.FromFile(flagPath));
+                int.TryParse(args[2], out pixelMargin);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: ".Pastel(Color.DarkRed) + ex.Message.Pastel(Color.Red));
+                Environment.Exit(1);
+            }
+            WriteHeaders(args[1]);
+            Console.WriteLine("\nMaking image...".Pastel(Color.LightGreen));
+
+            Bitmap croppedBmp = CropPicture(ref imageFile, 800);
+            Bitmap flagBmp = CropFlag(ref flag, pixelMargin);
+            Bitmap finalBmp = StitchTogether(ref flagBmp, ref croppedBmp);
+
+            try { finalBmp.Save(args[3], ImageFormat.Png); }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: ".Pastel(Color.DarkRed) +
+                    ex.StackTrace.Pastel(Color.Red));
+                Environment.Exit(1);
+            }
+            Console.WriteLine($"Success! Saved image \"{args[3]}\"".Pastel(Color.SpringGreen));
+            Console.WriteLine($"FlagPFP, by Aesthetical#9203, 2021.".Pastel(Color.PaleTurquoise));
+            Environment.Exit(0);
+        }
+    }
+}
